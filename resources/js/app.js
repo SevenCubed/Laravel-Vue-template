67a48ee228d6fad85ff9fea98736d4dfbf7caf27
SevenCubed <78549436+SevenCubed@users.noreply.github.com>
2021-06-04 14:02:44 +0200

    Bulma integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,9 @@ import store from "./store";
 //Import router
 import router from "./router";
 
+//Import Bulma CSS
+import "./../../node_modules/Bulma/CSS/bulma.css";
+
 new Vue({
     el: "#app",
     store,
